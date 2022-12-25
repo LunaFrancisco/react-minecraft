9d3b416adad809eee933b3e67313c1a69722a61a
LunaFrancisco <francisco.luna@mail.udp.cl>
2022-12-25 11:46:32 -0300

    calling app js

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { Canvas } from '@react-three/fiber';
 import {Sky} from '@react-three/drei';
+import {Physics} from '@react-three/cannon';
+import {Ground} from './Components/Ground/Ground.jsx';
+import { FPV } from './Components/FPV/index.jsx';
  
 function App() {
 
@@ -8,6 +11,11 @@ function App() {
       <Canvas>
         <Sky sunPosition={[300,100,20]} />
         <ambientLight intensity={0.5}  />
+        <Physics>
+        <Ground/>
+        <FPV/>
+        </Physics>
+
         </Canvas>
     </>
   )
